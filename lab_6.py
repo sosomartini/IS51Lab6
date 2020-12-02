40f693fa12c58807ae74f1e8060390323a8be022
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-01 17:56:32 -0800

    add changes

--- lab_6.py
+++ lab_6.py
@@ -1,3 +1,13 @@
 """
+To start, we will generate a random interger between 1 and 20, and 
+based on the resut of the random number, we check to see if it falls under certain range 
+if the number is greater than 15, then the result will be "cherries"
+otherwise if the number is > 10, then the result will be "orange"
+otherwise if the number is > 5, then the result will be "Plum"
+otherwise if the number is > 2, then the result will be "Melon"
+otherwise if the number is > 1, then the result will be "Bell"
+If the number is not any of the above, the number would be "Bar"
 
+
+we iterate over using a loop threee times and print the results to the user. As an example "Plum Cherries"
 """
